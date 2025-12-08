1e404c21a542ba1a671aeea53b3e2b2be5a129c2
AmbroiseJuston <116405013+AmbroiseJuston@users.noreply.github.com>
2025-12-08 13:15:44 -0500

    Delete solutionItteration.m

--- solutionItteration.m
+++
@@ -1,7 +0,0 @@
-for r = 1.5:0.1:10
-    solinit.x = sol.x;
-    solinit.y = sol.y;
-    solinit.parameters = sol.parameters;
-    sol = bvp4c(@(t,s,times)ODEMain(t,s,times,r), @(so,sf,times)bcfunc(so,sf,times, r, points), solinit, options);
-    fprintf("final time  = %2.4f\n", sum(sol.parameters))
-end
