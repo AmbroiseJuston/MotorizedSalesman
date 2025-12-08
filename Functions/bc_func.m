0e8858bd8a63e82ba28d6785b0950be8d2cddb06
AmbroiseJuston <116405013+AmbroiseJuston@users.noreply.github.com>
2025-12-08 15:05:31 -0500

    Update bc_func.m
    fixing typo

--- Functions/bc_func.m
+++ Functions/bc_func.m
@@ -14,7 +14,7 @@ for i = 1:(length(times)-1)
     con(9*i-4:9*i-1) = sf(1+8*(i-1):4+8*(i-1)) - so(8*i+1:8*i+4); % x, y, theta, v continuous
     con(9*i:9*i+1) = [sf(1+8*(i-1)) - cities(1,i)
                       sf(2+8*(i-1)) - cities(2,i)]; % Goes through cities
-    con(9*i+2:9*i+3) = sf(7+8*(i-1):8+8*(i-1)) - s0(8*i+7:8*i+8); % lambda_theta and lambda_v continuous
+    con(9*i+2:9*i+3) = sf(7+8*(i-1):8+8*(i-1)) - so(8*i+7:8*i+8); % lambda_theta and lambda_v continuous
     con(9*i+4) = Hamiltonian(sf(1+8*(i-1)):8+8*(i-1),r); % Hamiltonian = 0
 end
 
@@ -23,4 +23,5 @@ con(9*i+6) = sf(end-6) - cities(2,i); % y(tf) = cities(2,i)
 con(9*i+7) = sf(end-1);    % lambda_theta(tf) = 0
 con(9*i+8) = sf(end);    % lambda_v(tf) = 0
 con(9*i+9) = Hamiltonian(sf(end-7:end), r); % Hamiltonian(tf) = 0
+
 end
